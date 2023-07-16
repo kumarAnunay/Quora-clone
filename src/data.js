b9b3b6cb79e30cfcaac2151fcc06a0165ba908a8
kumarAnunay <kumaranunay12@gmail.com>
2023-07-16 17:35:42 +0530

    Completed

--- src/data.js
+++ src/data.js
@@ -1,96 +1,114 @@
 export const questionAndAnswers = [
-    {
-        id: "1",
-        answeredBy: "Aman",
-        questionedBy: "Aman Sen",
-        question: "What is the famous food of Agra?",
-        answer: "Petha is the famous sweet of Agra",
-
-    },
-    {
-        id: "2",
-        answeredBy: "Nikhil",
-        questionedBy: "Amartya",
-        question: "What color is the sky?",
-        answer: "The sky is of color blue.The color is blue because The Raman Effect",
-
-    },
-    {
-        id: "3",
-        answeredBy: "Nitish",
-        questionedBy: "Abhishek",
-        question: "When did the world war start?",
-        answer: "The world war 1 started in 1914",
-
-    },
-    {
-        id: "4",
-        answeredBy: "Shubham",
-        questionedBy: "Ankit",
-        question: 'Which animal is known as the "Ship of the Desert"?',
-        answer: "Camel",
-
-    },
-    {
-        id: "4",
-        answeredBy: "Abhishek",
-        questionedBy: "Amartya",
-        question: "How many consonants are there in the English alphabet?",
-        answer: "21 Consonants",
-
-    }
+  {
+    id: "1",
+    answeredBy: "Aman",
+    questionedBy: "Aman Sen",
+    question: "What is the famous food of Agra?",
+    answer: "Petha is the famous sweet of Agra",
+  },
+  {
+    id: "2",
+    answeredBy: "Nikhil",
+    questionedBy: "Amartya",
+    question: "What color is the sky?",
+    answer:
+      "The sky is of color blue.The color is blue because The Raman Effect",
+  },
+  {
+    id: "3",
+    answeredBy: "Nitish",
+    questionedBy: "Abhishek",
+    question: "When did the world war start?",
+    answer: "The world war 1 started in 1914",
+  },
+  {
+    id: "4",
+    answeredBy: "Shubham",
+    questionedBy: "Ankit",
+    question: 'Which animal is known as the "Ship of the Desert"?',
+    answer: "Camel",
+  },
+  {
+    id: "4",
+    answeredBy: "Abhishek",
+    questionedBy: "Amartya",
+    question: "How many consonants are there in the English alphabet?",
+    answer: "21 Consonants",
+  },
+  {
+    id: "5",
+    answeredBy: "Amartya",
+    questionedBy: "Nitish",
+    question: "Who designed the National Flag of India?",
+    answer: "Pingali Venkayya",
+  },
 ];
 
 export const questionLists = [
-    {
-        id: '1',
-        questionedBy: 'Kamal rastogi',
-        question: "What is the famous food of Agra?",
-    },
-    {
-        id: "2",
-        questionedBy: "Rahul Kumar",
-        question: "What color is the sky?",
-    },
-    {
-        id: "3",
-        questionedBy: "Jatin Rawat",
-        question: "When did the world war start?",
-    },
-    {
-        id: "4",
-        questionedBy: "Jatin Rawat",
-        question: 'Which animal is known as the "Ship of the Desert"?',
-
-    },
-    {
-        id: "5",
-        questionedBy: "Rahul Kumar",
-        question: "Difference between strees and strain?",
-    },
-    {
-        id: "6",
-        questionedBy: "Abhishek",
-        question: "How many hours are there in a day?",
-    },
-    {
-        id: "7",
-        questionedBy: "Amartya",
-        question: "Rainbow consist of how many colours?",
-    },
-    {
-        id: "8",
-        questionedBy: "Shubham",
-        question: "How many days are there in a year?",
-    },
-    {
-        id: "9",
-        questionedBy: "Shivani",
-        question: "How many vowels are there in the English alphabet and name them?",
-    },
-    {
-        id: "10",
-        questionedBy: "Astha",
-        question: "Who designed the National Flag of India?",
-    }
+  {
+    id: "1",
+    questionedBy: "Kamal rastogi",
+    question: "What is the famous food of Agra?",
+  },
+  {
+    id: "2",
+    questionedBy: "Rahul Kumar",
+    question: "What color is the sky?",
+  },
+  {
+    id: "3",
+    questionedBy: "Jatin Rawat",
+    question: "When did the world war start?",
+  },
+  {
+    id: "4",
+    questionedBy: "Jatin Rawat",
+    question: 'Which animal is known as the "Ship of the Desert"?',
+  },
+  {
+    id: "5",
+    questionedBy: "Rahul Kumar",
+    question: "Difference between strees and strain?",
+  },
+  {
+    id: "6",
+    questionedBy: "Abhishek",
+    question: "How many hours are there in a day?",
+  },
+  {
+    id: "7",
+    questionedBy: "Amartya",
+    question: "Rainbow consist of how many colours?",
+  },
+  {
+    id: "8",
+    questionedBy: "Shubham",
+    question: "How many days are there in a year?",
+  },
+  {
+    id: "9",
+    questionedBy: "Shivani",
+    question:
+      "How many vowels are there in the English alphabet and name them?",
+  },
+  {
+    id: "10",
+    questionedBy: "Astha",
+    question: "Who designed the National Flag of India?",
+  },
+  {
+    id: "11",
+    questionedBy: "Shivani",
+    question: "Which planet is known as the Red Planet?",
+  },
+  {
+    id: "12",
+    questionedBy: "Amartya",
+    question: "Which is the largest plateau in the world?",
+  },
+  {
+    id: "13",
+    questionedBy: "Aman Sen",
+    question: "Which is the tallest mountain in the world?",
+  },
 ];
